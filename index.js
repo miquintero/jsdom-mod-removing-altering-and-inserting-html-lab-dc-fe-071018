b4b0743f245893a3fcf07a3bb4b57da472a856bc
Alexa Schaeffer <alps231@gmail.com>
2018-08-18 01:04:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 var body = document.createElement("body")
+for (let i = 0; i < 0; i++) {
+  let H1 = document.createElement('H1')
+  H1.innerHTML = (i + 1).toString()
+  body.appendChild(H1)
+}
+ 
+element.appendChild(H1)
