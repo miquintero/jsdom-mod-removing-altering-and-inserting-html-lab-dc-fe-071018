6267f0077bf1f70b20e5a0979d27a62955da4911
Alexa Schaeffer <alps231@gmail.com>
2018-08-18 01:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
-var body = document.createElement("newHeader")
-for (let i = 0; i < 0; i++) {
-  let newHeader = document.createElement('h1#victory')
-  newHeader.innerHTML = (i + 1).toString()
-  body.appendChild(H1)
-}
 
-element.appendChild(H1)
+let newHeader = document.createElement("h1");
+newHeader.innerText = Alexa is the champion; 
+body.appendChild(newHeader)
+
