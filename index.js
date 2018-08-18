d05ed20d338c19447b1a6a8d6b6d881137e9ddfb
Alexa Schaeffer <alps231@gmail.com>
2018-08-18 01:38:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
-
+document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerText = "Alexa is the champion!";
