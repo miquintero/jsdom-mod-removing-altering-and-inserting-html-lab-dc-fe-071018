2ec419791a1b6438acac239e21758e6323b55695
Alexa Schaeffer <alps231@gmail.com>
2018-08-18 00:56:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Enter the code to remove the main node element under this comment */
-body.remove(main)
+main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 document.createElement(H1)
